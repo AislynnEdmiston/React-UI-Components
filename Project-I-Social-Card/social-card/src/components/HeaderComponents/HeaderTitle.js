e99a7652bbb102e17e2e08b93806fa90337be6ec
Aislynn E <davidandaislynn@gmail.com>
2019-03-14 23:21:15 -0400

    Worked on getting the compents built and imported/exported into app

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div>
+            <h3>Lambda School</h3>
+            <span>@LambdaSchool</span>
+            <span>•</span>
+            <span>26 jan</span>
+        </div>
+    )
+}
+
+export default HeaderTitle;
