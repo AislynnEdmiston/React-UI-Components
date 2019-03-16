1344f31d912f8a4d4766d9907fdc16fdb670b647
Aislynn E <davidandaislynn@gmail.com>
2019-03-16 12:11:10 -0400

    tried moving with styling

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -8,7 +8,7 @@ const HeaderContainer = () => {
       <div>
        
        <HeaderTitle />
-       <HeaderContent />
+       
       </div>
     );
   };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
 
 const HeaderTitle = () => {
     return (
         <div className="header-title">
             <ImageThumbnail />
-            <div className='header-title-text'>
-                <h3>Lambda School</h3>
-                <span>@LambdaSchool</span>
-                <span>•</span>
-                <span>26 jan</span>
-            </div>
             
+            <h3>Lambda School</h3>
+            <span>@LambdaSchool</span>
+            <span>•</span>
+            <span>26 jan</span>
+            
+            <HeaderContent />
         </div>
     )
 }
